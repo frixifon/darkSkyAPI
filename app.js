2443ad380e69965dd8778fdc63e39132a543f34d
frixifon <38159256+frixifon@users.noreply.github.com>
2019-10-21 10:01:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', ()=>{
     let long = 24.7037952;
     let lat = 59.4075648;
-    //let long;
-    //let lat;
+  /*   let long;
+    let lat; */
 
-    if("geolocation" in navigator){
+  /*   if("geolocation" in navigator){
         long = position.coords.longitude;
         lat = position.coords.latitude;
         console.log("Long: ", long);
         console.log("Lat: ", lat);
     }
-
+ */
     /* if(navigator.geolocation){
         navigator-geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
@@ -20,6 +20,14 @@ window.addEventListener('load', ()=>{
         })
     }
  */
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api= `${proxy}https://api.darksky.net/forecast/07983bf47cebd8108d5c937972f8b874/${lat},${long}`;
+    
+    fetch(api)
+    .then(response => {
+       return response.json(); 
+    });
 
- const proxy
+
+
 });
