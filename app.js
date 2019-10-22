69e7d0dcb06d5bdf9fd5652dff037b44f8da514f
frixifon <38159256+frixifon@users.noreply.github.com>
2019-10-22 15:47:58 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 window.addEventListener('load', ()=>{
     let long = 24.7037952;
     let lat = 59.4075648;
+
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperaturDescription = document.querySelector(".temperature-description");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let degreeSection = document.querySelector(".degree-section");
+    let temperatureSectionSpan = document.querySelector(".degree-section span");
   /*   let long;
     let lat; */
 
@@ -17,17 +23,34 @@ window.addEventListener('load', ()=>{
             lat = position.coords.latitude;
             console.log("Long: ", long);
             console.log("lat: ", lat);
+
         })
     }
  */
     const proxy = "https://cors-anywhere.herokuapp.com/";
-    const api= `${proxy}https://api.darksky.net/forecast/07983bf47cebd8108d5c937972f8b874/${lat},${long}`;
+    const api= `${proxy}https://api.darksky.net/forecast/69fb9727b4fbc09bc9578c7228de41ca/${lat},${long}`;
     
     fetch(api)
     .then(response => {
        return response.json(); 
-    });
+    })
+    .then(data => {
+        const {temperature, summary, icon} = data.currently;
+        temperatureDegree.textContent = temperature;
+        temperaturDescription.textContent = summary;
+        locationTimezone.textContent = data.timezone;
+        
+        let celsius = (temperature - 32)* (5/9);
 
-
-
+        temperatureSectionSpan.addEventListener("click", () => {
+            if(temperatureSectionSpan.textContent == "F"){
+                temperatureSectionSpan.tetContent = "C";
+                temperatureDegree.textContent = Math.floor(celsius);
+            }
+            else{
+                temperatureSectionSpan.tetContent = "F";
+                temperatureDegree.textContent = temperature;
+            }
+        })
+    })
 });
